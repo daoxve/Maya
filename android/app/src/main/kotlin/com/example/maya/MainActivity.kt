ada91414a6f568ba518a0d5cf8162af3bc506471
David Coker <heiscokerdavid@gmail.com>
2021-10-09 11:32:59 +0100

    🔥 Initial commit

---
+++ android/app/src/main/kotlin/com/example/maya/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.maya
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: null,
+    ),
+  );
+}
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
