4484ed0cd4610b8871dce0995583a107b87169c9
David Coker <heiscokerdavid@gmail.com>
2021-10-09 12:02:54 +0100

    🛠️ Added basic functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,45 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      home: null,
-    ),
-  );
-}
+void main() => runApp(
+      MaterialApp(
+        home: BallPage(),
+      ),
+    );
 
 class BallPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: const Text('Ask Me Anything'),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+          });
+        },
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
+    );
   }
 }
